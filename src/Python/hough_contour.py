a60bdda5afdf58129c8e39abd175407e795f9d10
Ivan Dardi <ivy.dardi@gmail.com>
2016-08-10 17:13:31 -0300

    Moving Over to C++

--- src/Python/hough_contour.py
+++ src/Python/hough_contour.py
@@ -27,13 +27,16 @@ class HoughContour(srpv):
         cv2.imwrite("{0}/{1}_preprocessing3.jpg".format(self.image.path_save, self.image.name), img_canny)
 
         img_contours, contours, hierarchy = cv2.findContours(img_canny.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        img_filled = np.zeros_like(self.img_gray)
+        cv2.drawContours(img_filled, contours, -1, white, 2)
+        cv2.imwrite("{0}/{1}_preprocessing4.jpg".format(self.image.path_save, self.image.name), img_filled)
 
         img_filled = np.zeros_like(self.img_gray)
         for cnt in contours:
             # shapes = cv2.approxPolyDP(cnt, 1.0, True)
             shape = cv2.convexHull(cnt, returnPoints=True)
             cv2.fillPoly(img_filled, shape, white)
-        cv2.imwrite("{0}/{1}_preprocessing4.jpg".format(self.image.path_save, self.image.name), img_filled)
+        cv2.imwrite("{0}/{1}_preprocessing5.jpg".format(self.image.path_save, self.image.name), img_filled)
 
         lines = cv2.HoughLines(img_filled, 1, np.pi / 180, 75)
         try:
@@ -61,7 +64,7 @@ class HoughContour(srpv):
         except Exception as e:
             print(e)
 
-        cv2.imwrite("{0}/{1}_preprocessing5.jpg".format(self.image.path_save, self.image.name), img_contours)
+        cv2.imwrite("{0}/{1}_preprocessing6.jpg".format(self.image.path_save, self.image.name), img_contours)
 
         # img_rect = self.img_orig.copy()
         # img_cont = np.zeros_like(self.img_orig)
@@ -70,43 +73,4 @@ class HoughContour(srpv):
         # cv2.imwrite("{0}{1}_processing_rectangle.jpg".format(self.dst, self.img_name, i), img_rect)
 
     def crop(self):
-        for j, cnt in enumerate(cnt):
-            plate_ratio = float(w) / h
-            area = w * h
-            if AP1M < area < AP1P:
-                if RP1M < plate_ratio < RP1P:
-                    cv2.imwrite("{}/crops/{}_canny{}_{}.jpg".format(self.dst, self.img_name, i, j),
-                                img_crop[y:y + h, x:x + w])
-                    cv2.rectangle(img_rect, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            elif AP2M < area < AP2P:
-                if RP2M < plate_ratio < RP2P:
-                    cv2.imwrite("{}/crops/{}_canny{}_{}.jpg".format(self.dst, self.img_name, i, j),
-                                img_crop[y:y + h, x:x + w])
-                    cv2.rectangle(img_rect, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            elif AP3M < area < AP3P:
-                if RP3M < plate_ratio < RP3P:
-                    cv2.imwrite("{}/crops/{}_canny{}_{}.jpg".format(self.dst, self.img_name, i, j),
-                                img_crop[y:y + h, x:x + w])
-                    cv2.rectangle(img_rect, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            cv2.imwrite("{}/crops/{}_hough{}.jpg".format(self.dst, self.img_name, i), img_rect)
-
-        img_contours, contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        shapes = cv2.approxPolyDP(contours, 0.1 * cv2.arcLength(contours, True), True)
-        for j, shape in enumerate(shape):
-            if len(shape) == 4:
-                [x, y, w, h] = cv2.boundingRect(cnt)
-                ratio = float(w) / h
-                if 2.6 < ratio < 3.4:
-                    if hry[2] >= 0:
-                        cv2.rectangle(img_rect, (x, y), (x + w, y + h), green, 1)  # CLOSED
-                        cv2.drawContours(img_cont, contours, j, white)
-                    else:
-                        cv2.rectangle(img_rect, (x, y), (x + w, y + h), red, 1)  # OPEN
-                        cv2.drawContours(img_cont, contours, j, blue)
-
-        img_contours, contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        for j, cnt in enumerate(contours):
-            shape = cv2.approxPolyDP(cnt, 0.05 * cv2.arcLength(cnt, True), True)
-            if len(shape) == 4:
-                draw_rect(img_rect, shape, red)
-                cv2.drawContours(img_cont, [shape], 0, white)
+        pass
